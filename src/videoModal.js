30bdc8869262a6034eb23ac7da909f53eb0e62b2
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2024-03-15 12:27:21 +0600

    modal issue fixed

--- src/videoModal.js
+++ src/videoModal.js
@@ -1,11 +1,17 @@
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("#video-modal-open");
+const showButton2 = document.querySelector("#video-modal-open2");
+const showButton3 = document.querySelector("#video-modal-open3");
 const closeButton = document.querySelector("dialog button");
 
-showButton.addEventListener("click", () => {
+showButton.addEventListener("click", handleShowButton);
+showButton2.addEventListener("click", handleShowButton);
+showButton3.addEventListener("click", handleShowButton);
+
+function handleShowButton() {
   dialog.showModal();
   document.body.style.overflow = "hidden";
-});
+}
 
 closeButton.addEventListener("click", () => {
   dialog.close();
