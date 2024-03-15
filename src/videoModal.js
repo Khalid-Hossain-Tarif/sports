ed5d21791181633730851e6aca91534ac5504c44
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2024-03-15 16:07:46 +0600

    video modal and play issue fixed

--- src/videoModal.js
+++ src/videoModal.js
@@ -1,19 +1,23 @@
 const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("#video-modal-open");
+const showButton0 = document.querySelector("#video-modal-open0");
+const showButton1 = document.querySelector("#video-modal-open1");
 const showButton2 = document.querySelector("#video-modal-open2");
-const showButton3 = document.querySelector("#video-modal-open3");
 const closeButton = document.querySelector("dialog button");
+const iframe = document.querySelector(".modal-video");
 
-showButton.addEventListener("click", handleShowButton);
+showButton0.addEventListener("click", handleShowButton);
+showButton1.addEventListener("click", handleShowButton);
 showButton2.addEventListener("click", handleShowButton);
-showButton3.addEventListener("click", handleShowButton);
 
 function handleShowButton() {
   dialog.showModal();
   document.body.style.overflow = "hidden";
+  iframe.pause()
 }
 
 closeButton.addEventListener("click", () => {
   dialog.close();
   document.body.style.overflow = "auto";
+  let src = iframe.src;
+	iframe.src = src;
 });
